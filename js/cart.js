ac573f6b1b01410cb2699653169a41ddfc56c1ce
Angels101 <angelscodex101@gmail.com>
2020-08-07 02:01:32 +0300

    defining the function with ohter properties

--- js/cart.js
+++ js/cart.js
@@ -12,3 +12,22 @@ function Results (type,size,crust,toppings) {
 Results.prototype.order = function() {
     return "You have made an order of " + this.type + " pizza  with " + this.toppings + " as toppings and " + this.crust + " for crust ."
 };
+function Results (type,size,crust,toppings) {
+    this.type = type;
+    this.size= size;
+    this.crust= crust;
+    this.toppings=toppings;
+};
+Results.prototype.order = function() {
+    return "You have made an order of " + this.type + " pizza  with " + this.toppings + " as toppings and " + this.crust + " for crust ."
+};
+function TotalPrice (price, quantity, delivery,toppings,crust) {
+    this.price= price;
+    this.quantity=quantity;
+    this.delivery=delivery;
+    this.toppings=toppings;
+    this.crust=crust;
+};
+TotalPrice.prototype.finalTotal = function () {
+    return ( this.price + this.delivery + this.toppings + this.crust )* this.quantity ;
+};
