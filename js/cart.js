e8229842025b9b4db290c488492a9db631b90baa
Angels101 <angelscodex101@gmail.com>
2020-08-07 02:06:04 +0300

    relization of more than one function

--- js/cart.js
+++ js/cart.js
@@ -31,3 +31,29 @@ function TotalPrice (price, quantity, delivery,toppings,crust) {
 TotalPrice.prototype.finalTotal = function () {
     return ( this.price + this.delivery + this.toppings + this.crust )* this.quantity ;
 };
+var sizePrices = [1200, 800, 600]
+var priceToppings = [100,120,80,150,200]
+var toppingsName= ["Pepperoni" , "mushroom" , "onion" ,"sausage", " bacon"]
+var crustNames= ["Crispy", "Stuffed", "Glutton-free"]
+var crustPrices = [100,120,200]
+var deliveryPrices = [0, 200];
+$(document).ready(function(){
+    $('form#myForm').submit(function (event) {
+        event.preventDefault();
+        var pizzaType = $('#type').val();
+        var pizzaSize = parseInt($('#size').val());
+        var pizzaToppings=parseInt($('#top').val());
+        var priceCrust =parseInt($('#crusting').val());
+        var pizzaTop = $('#top').val();
+        var pizzaQty = parseInt($('#quantity').val());
+        var pizzaPick = parseInt($('#delivery').val());
+        var price = sizePrices[pizzaSize - 1];
+        var DeliveryCost = deliveryPrices[pizzaPick - 1];
+        var ToppingsCost = priceToppings[pizzaToppings-1];
+        var crustCost = crustPrices[priceCrust-1]
+        var topNames = toppingsName[pizzaTop-1]
+        var crustName = crustNames[priceCrust-1]
+
+        newOrder = new Results(pizzaType,pizzaSize, crustName,topNames,crustName);
+        newTotal = new TotalPrice(price, pizzaQty, DeliveryCost,ToppingsCost,crustCost);
+        
